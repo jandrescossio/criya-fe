b0118b2fb98387f1da2dd479321d86a6937f298b
Jorge Andres Lopez Cossio <alopezcossio@gmail.com>
2022-05-23 00:42:33 -0400

    #2rxcnjy chore: extend configuration

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    apiUrl: 'http://localhost/api',
+    storageUrl: 'http://localhost/storage',
+  },
+  images: {
+    domains: ['localhost'],
+  },
 }
 
 module.exports = nextConfig
